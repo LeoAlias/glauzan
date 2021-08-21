428fc9d17c76100717461bdd6db6868830f06441
LeoAlias <leo.alias@gmail.com>
2021-08-20 22:50:34 -0300

    se modificaron las funcionalidades de envio de mail

--- quote.php
+++ quote.php
@@ -1,15 +1,3 @@
-<!-- <?php
-
-  $name = $_POST['name'];
-  $email = $_POST['email'];
-  $subject = "Formulario de contacto: ".$_POST['subject'];
-  $message = $_POST['message'];
-  $recipient = "leo.alias@gmail.com";
-  $mailheader = "From: $email \r\n";
-  $formcontent=" De: $name \n Mensaje: $message";
-  mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-
-?> -->
 <?php
 $name = $_POST['name'];
 $mail = $_POST['mail'];
@@ -22,7 +10,6 @@ $header .= "Content-Type: text/plain";
 
 $message = "Este mensaje fue enviado por: " . $name . " \r\n";
 $message .= "Su e-mail es: " . $mail . " \r\n";
-$message .= "Teléfono de contacto: " . $phone . " \r\n";
 $message .= "Mensaje: " . $_POST['message'] . " \r\n";
 $message .= "Enviado el: " . date('d/m/Y', time());
 
@@ -33,4 +20,3 @@ mail($para, $asunto, utf8_decode($message), $header);
 
 header("Location:index.html");
 ?>
-
